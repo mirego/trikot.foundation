a8c4d85783b5eb77614babf2c786dd09a6a334d9
Charles MacDuff <cmacduff@mirego.com>
2019-10-11 11:39:59 -0400

    Linter

--- foundation/src/jvmMain/kotlin/com/mirego/trikot/foundation/date/Date.kt
+++ foundation/src/jvmMain/kotlin/com/mirego/trikot/foundation/date/Date.kt
@@ -4,13 +4,13 @@ import org.threeten.bp.Instant
 import org.threeten.bp.OffsetDateTime.from
 import org.threeten.bp.ZoneOffset
 import org.threeten.bp.format.DateTimeFormatter
-import java.util.*
+import java.util.Locale
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 private val dateFormat =
     DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
-        .withZone( ZoneOffset.UTC )
+        .withZone(ZoneOffset.UTC)
 
 @ExperimentalTime
 actual class Date(val instant: Instant) {
