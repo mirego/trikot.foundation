faeced375ef80cd15e21a1991b4cdd50bcfb468b
Antoine Lamy <antoinelamy@gmail.com>
2021-02-25 15:52:40 -0500

    Remove duplicated classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ buildscript {
     }
 
     dependencies {
-        this.classpath("com.android.tools.build:gradle:4.1.2")
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlin_version"]}")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
