c050444dd94cf53a1dfad6f6dee30b41b41aa15e
Nicolas Presseault <npresseault@mirego.com>
2021-08-31 17:42:49 -0400

    Deactivate freeze

--- trikotFoundation/src/nativeMain/kotlin/com/mirego/trikot/foundation/concurrent/MrFreeze.kt
+++ trikotFoundation/src/nativeMain/kotlin/com/mirego/trikot/foundation/concurrent/MrFreeze.kt
@@ -1,15 +1,12 @@
 package com.mirego.trikot.foundation.concurrent
 
-import kotlin.native.concurrent.ensureNeverFrozen
-import kotlin.native.concurrent.freeze
-
 actual object MrFreeze {
     actual fun <T> freeze(objectToFreeze: T): T {
-        return objectToFreeze.freeze()
+        return objectToFreeze
     }
 
     actual fun ensureNeverFrozen(objectToProtect: Any) {
-        objectToProtect.ensureNeverFrozen()
+        objectToProtect
     }
 }
 
